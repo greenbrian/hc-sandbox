bcaa69fa04245e249869b3282212a8f9b69740ec
Brian Green <bgreen@hashicorp.com>
2016-11-03 20:21:52 -0500

    updated script to update ubuntu

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -3,5 +3,6 @@
 # update system and install packages
 sudo DEBIAN_FRONTEND=noninteractive apt-get update --yes -qq
 sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade --yes -qq
+sudo DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade --yes -qq
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --yes \
 --force-yes -q curl wget unzip iptables-persistent jq
